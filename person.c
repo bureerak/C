dcef8edbacee01d5de63e6ffb3a4eb36e2fbbf52
bureerak <pongphon_brr@hotmail.com>
2025-07-10 20:50:24 +0700

    minor change

--- person.c
+++ person.c
@@ -7,9 +7,10 @@ int main() {
     char person3[80];
     scanf("%s",fname1);
     scanf("%s",sname1);
-    scanf("%[^\n]",person2);
-    scanf("%[^\n]",person3);
+    scanf(" %[^\n]",person2);
+    scanf(" %[^\n]",person3);
     printf("Person 1: %s %s\n",fname1,sname1);
     printf("Person 2: %s\n",person2);
     printf("Person 3: %s",person3);
+    return 0;
 }
