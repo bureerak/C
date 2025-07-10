a85e9843acef304e1ffbbe075aefdafdcb762421
bureerak <pongphon_brr@hotmail.com>
2025-07-10 11:08:16 +0700

    update

---
+++ person.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char fname1[30];
+    char sname1[30];
+    char person2[80];
+    char person3[80];
+    scanf("%s",fname1);
+    scanf("%s",sname1);
+    scanf("%[^\n]",person2);
+    scanf("%[^\n]",person3);
+    printf("Person 1: %s %s\n",fname1,sname1);
+    printf("Person 2: %s\n",person2);
+    printf("Person 3: %s",person3);
+}
